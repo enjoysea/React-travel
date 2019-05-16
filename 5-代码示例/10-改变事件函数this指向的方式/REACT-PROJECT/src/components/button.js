6c5e5baf1d8c0df24a76b99d2b45a2d3d38b6f53
enjoysea <779630561@qq.com>
2019-05-16 10:53:55 +0800

    修改this指向

--- 5-代码示例/10-改变事件函数this指向的方式/REACT-PROJECT/src/components/button.js
+++ 5-代码示例/10-改变事件函数this指向的方式/REACT-PROJECT/src/components/button.js
@@ -6,9 +6,11 @@ import React from 'react'
 class Button extends React.Component {
   
   constructor(props) {
+    console.log('#', props)
     super(props)
     // 给实例上添加一个方法
     // 初始化去改变this指向
+    // 改变this指向方式二
     this.handle4 = this.handle4.bind(this)
   }
   
@@ -44,7 +46,8 @@ class Button extends React.Component {
   handle6 = (e) => {
     console.log('事件对象', e)
     e.persist() // 保持引用
-    console.log({...e}) // 有值
+    // 技巧：浅克隆一份。有值
+    console.log({...e})
     console.log(e.type)
     
     // 保持对事件对象的引用
@@ -69,28 +72,34 @@ class Button extends React.Component {
         <button onClick={() => {
           console.log(1)
         }}>{value}</button>
-        {/*测试函数中this指向的问题*/}
+        {/* 测试函数中this指向的问题 */}
         <button onClick={this.handle2}>测试类中函数this指向</button>
         <button onClick={() => {
           console.log(this)
         }}>{value}</button>
-        {/*修改函数中this指向问题*/}
+        {/* 修改函数中this指向问题 */}
         
-        {/*bind调用后会返回一个新的函数，
+        {/* bind调用后会返回一个新的函数，
          点击button后就会将新函数给执行，
          在内部里面会将handle3给执行了，
-         handle3的this，就是bind的第一个参数*/}
+         handle3的this，就是bind的第一个参数 */}
+         
+        {/* 以下bind的方式也是有问题的，因为是放在render中，每一次更新，bind 都会重新调用*/}
+        {/* 改变this指向方式一 */}
         <button onClick={this.handle3.bind(this)}>修改函数中this指向3</button>
         <button onClick={this.handle4}>修改函数中this指向4</button>
         <button onClick={this.handle5}>修改函数中this指向5</button>
-        {/*测试事件对象*/}
+        {/* 测试事件对象 */}
         <button onClick={this.handle6}>测试事件对象</button>
-        {/*给事件对象传参*/}
+    
+        {/* 给事件对象传参 */}
         <button onClick={this.handle7.bind(this, {a: 1, b: 2})}>给事件对象传参</button>
+        {/* 事件对象手动的传过去 */}
         <button onClick={(e) => {
           this.handle7({a: 1, b: 2}, e)
         }}>给事件对象传参
         </button>
+    
       </React.Fragment>
     )
   }
